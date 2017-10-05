bc5a5d30b4a398629d1332b94c6969345048b8b8
Matthew Li <li.matthew.m@gmail.com>
2017-10-05 16:16:02 -0700

    Added info service controller and fixed some naming conventions with Airplane and Airports

---
+++ application/controllers/Info.php
@@ -0,0 +1,43 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * A ser
+ */
+
+class Info extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function airplanes()
+    {
+        $this->load->model('airplanes');
+        $data = json_encode($this->airplanes->all());
+        return $data;
+    }
+
+    function airports()
+    {
+        $this->load->model('airports');
+        $data = json_encode($this->airports->all());
+        return $data;
+    }
+
+    function fleet()
+    {
+        $this->load->model('fleet');
+        $data = json_encode($this->fleet->all());
+        return $data;
+    }
+
+    function flights()
+    {
+        $this->load->model('flights');
+        $data = json_encode($this->flights->all());
+        return $data;
+    }
+}
