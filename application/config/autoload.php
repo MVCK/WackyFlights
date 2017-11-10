c4be0bfbe578378901efcf05bbc74343dd13d947
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-10 13:21:42 -0800

    Closes #34: Added Plane entity model and updated autoload.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Airplanes', 'Airports', 'Flights', 'Flight');
+$autoload['model'] = array('Airplanes', 'Airports', 'Flights', 'Entity', 'Flight', 'Plane');
