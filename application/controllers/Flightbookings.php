61f00457bdbe6f941b6763e662c7702993938d0e
Vincent Lee <vincent.h.lee@outlook.com>
2017-11-11 15:27:21 -0800

    Closes #41: Flight bookings depart/arrive at same airport problem fixed. Migrated to CSV_Model as well.

--- application/controllers/Flightbookings.php
+++ application/controllers/Flightbookings.php
@@ -27,25 +27,27 @@ class Flightbookings extends Application
 
         $departure = $data['departureAirport'];
         $arrival = $data['arrivalAirport'];
+
         $this->load->model('flights');
         $flights = $this->flights->all();
 
         $matches = array();
-
-        foreach($flights as $flight) {
-            if($flight['departureAirport'] == $departure) {
-                if($flight['arrivalAirport'] == $arrival) {
-                    array_push($matches, array($flight)); //direct flight
-                } else {
-                    foreach($flights as $flight2) {
-                        if($flight2['departureAirport'] == $flight['arrivalAirport']) {
-                            if($flight2['arrivalAirport'] == $arrival) {
-                                array_push($matches, array($flight, $flight2)); //check one stop
-                            } else {
-                                foreach($flights as $flight3) {
-                                    if($flight3['departureAirport'] == $flight2['arrivalAirport']) {
-                                        if($flight3['arrivalAirport'] == $arrival) {
-                                            array_push($matches, array($flight, $flight2, $flight3));
+        if($departure != $arrival) {
+            foreach($flights as $flight) {
+                if($flight->departureAirport == $departure) {
+                    if($flight->arrivalAirport == $arrival) {
+                        array_push($matches, array($flight)); //direct flight
+                    } else {
+                        foreach($flights as $flight2) {
+                            if($flight2->departureAirport == $flight->arrivalAirport) {
+                                if($flight2->arrivalAirport == $arrival) {
+                                    array_push($matches, array($flight, $flight2)); //check one stop
+                                } else {
+                                    foreach($flights as $flight3) {
+                                        if($flight3->departureAirport == $flight2->arrivalAirport && $flight2->arrivalAirport != $departure) {
+                                            if($flight3->arrivalAirport == $arrival) {
+                                                array_push($matches, array($flight, $flight2, $flight3));
+                                            }
                                         }
                                     }
                                 }
